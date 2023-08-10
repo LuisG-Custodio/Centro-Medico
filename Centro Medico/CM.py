ebc283e84698c377ff1d3fcbe9b3e64edf8ebc4a
LuisG-Custodio <121039302@upq.edu.mx>
2023-08-09 22:52:16 -0600

    Second draft

--- Centro Medico/CM.py
+++ Centro Medico/CM.py
@@ -13,6 +13,7 @@ def index():
     if not session.get("name"):
         return redirect("/login")
     return render_template('index.html')
+    
  
  
 @app.route("/login", methods=["POST", "GET"])
@@ -20,7 +21,7 @@ def login():
     if request.method == "POST":
         session["name"] = request.form.get("name")
         return redirect("/")
-    return render_template("login.html")
+    return render_template("template_perfiles.html")
  
  
 @app.route("/logout")
